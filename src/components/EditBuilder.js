8742677e391a1f419cfb54c0893aa5778da7db7d
Poojapatil2 <153355822+Poojapatil2@users.noreply.github.com>
2023-12-22 15:38:36 +0530

    Add files via upload

--- src/components/EditBuilder.js
+++ src/components/EditBuilder.js
@@ -29,362 +29,6 @@ const EditBuilder = () => {
             setSchema(res.data.data)
         })
         .catch(err => console.log("error", err))
-        // const responsedata = {
-        //     components: [
-        //         {
-        //             "label": "Text Field",
-        //             "labelPosition": "top",
-        //             "placeholder": "",
-        //             "description": "",
-        //             "tooltip": "",
-        //             "prefix": "",
-        //             "suffix": "",
-        //             "widget": {
-        //                 "type": "input"
-        //             },
-        //             "inputMask": "",
-        //             "displayMask": "",
-        //             "applyMaskOn": "change",
-        //             "allowMultipleMasks": false,
-        //             "customClass": "",
-        //             "tabindex": "",
-        //             "autocomplete": "",
-        //             "hidden": false,
-        //             "hideLabel": false,
-        //             "showWordCount": false,
-        //             "showCharCount": false,
-        //             "mask": false,
-        //             "autofocus": false,
-        //             "spellcheck": true,
-        //             "disabled": false,
-        //             "tableView": true,
-        //             "modalEdit": false,
-        //             "multiple": false,
-        //             "persistent": true,
-        //             "inputFormat": "plain",
-        //             "protected": false,
-        //             "dbIndex": false,
-        //             "case": "",
-        //             "truncateMultipleSpaces": false,
-        //             "encrypted": false,
-        //             "redrawOn": "",
-        //             "clearOnHide": true,
-        //             "customDefaultValue": "",
-        //             "calculateValue": "",
-        //             "calculateServer": false,
-        //             "allowCalculateOverride": false,
-        //             "validateOn": "change",
-        //             "validate": {
-        //                 "required": false,
-        //                 "pattern": "",
-        //                 "customMessage": "",
-        //                 "custom": "",
-        //                 "customPrivate": false,
-        //                 "json": "",
-        //                 "minLength": "",
-        //                 "maxLength": "",
-        //                 "strictDateValidation": false,
-        //                 "multiple": false,
-        //                 "unique": false
-        //             },
-        //             "unique": false,
-        //             "errorLabel": "",
-        //             "errors": "",
-        //             "key": "textField",
-        //             "tags": [],
-        //             "properties": {},
-        //             "conditional": {
-        //                 "show": null,
-        //                 "when": null,
-        //                 "eq": "",
-        //                 "json": ""
-        //             },
-        //             "customConditional": "",
-        //             "logic": [],
-        //             "attributes": {},
-        //             "overlay": {
-        //                 "style": "",
-        //                 "page": "",
-        //                 "left": "",
-        //                 "top": "",
-        //                 "width": "",
-        //                 "height": ""
-        //             },
-        //             "type": "textfield",
-        //             "input": true,
-        //             "refreshOn": "",
-        //             "dataGridLabel": false,
-        //             "addons": [],
-        //             "inputType": "text",
-        //             "id": "ejqbdbb",
-        //             "defaultValue": null
-        //         },
-        //         {
-        //             "label": "Text Area",
-        //             "labelPosition": "top",
-        //             "placeholder": "",
-        //             "description": "",
-        //             "tooltip": "",
-        //             "prefix": "",
-        //             "suffix": "",
-        //             "widget": {
-        //                 "type": "input"
-        //             },
-        //             "displayMask": "",
-        //             "applyMaskOn": "change",
-        //             "editor": "",
-        //             "autoExpand": false,
-        //             "customClass": "",
-        //             "tabindex": "",
-        //             "autocomplete": "",
-        //             "hidden": false,
-        //             "hideLabel": false,
-        //             "showWordCount": false,
-        //             "showCharCount": false,
-        //             "autofocus": false,
-        //             "spellcheck": true,
-        //             "disabled": false,
-        //             "tableView": true,
-        //             "modalEdit": false,
-        //             "multiple": false,
-        //             "persistent": true,
-        //             "inputFormat": "html",
-        //             "protected": false,
-        //             "dbIndex": false,
-        //             "case": "",
-        //             "truncateMultipleSpaces": false,
-        //             "encrypted": false,
-        //             "redrawOn": "",
-        //             "clearOnHide": true,
-        //             "customDefaultValue": "",
-        //             "calculateValue": "",
-        //             "calculateServer": false,
-        //             "allowCalculateOverride": false,
-        //             "validateOn": "change",
-        //             "validate": {
-        //                 "required": false,
-        //                 "pattern": "",
-        //                 "customMessage": "",
-        //                 "custom": "",
-        //                 "customPrivate": false,
-        //                 "json": "",
-        //                 "minLength": "",
-        //                 "maxLength": "",
-        //                 "minWords": "",
-        //                 "maxWords": "",
-        //                 "strictDateValidation": false,
-        //                 "multiple": false,
-        //                 "unique": false
-        //             },
-        //             "unique": false,
-        //             "errorLabel": "",
-        //             "errors": "",
-        //             "key": "textArea",
-        //             "tags": [],
-        //             "properties": {},
-        //             "conditional": {
-        //                 "show": null,
-        //                 "when": null,
-        //                 "eq": "",
-        //                 "json": ""
-        //             },
-        //             "customConditional": "",
-        //             "logic": [],
-        //             "attributes": {},
-        //             "overlay": {
-        //                 "style": "",
-        //                 "page": "",
-        //                 "left": "",
-        //                 "top": "",
-        //                 "width": "",
-        //                 "height": ""
-        //             },
-        //             "type": "textarea",
-        //             "rows": 3,
-        //             "wysiwyg": false,
-        //             "input": true,
-        //             "refreshOn": "",
-        //             "dataGridLabel": false,
-        //             "allowMultipleMasks": false,
-        //             "addons": [],
-        //             "mask": false,
-        //             "inputType": "text",
-        //             "inputMask": "",
-        //             "fixedSize": true,
-        //             "id": "e0zx2zl",
-        //             "defaultValue": null
-        //         },
-        //         {
-        //             "label": "Select Boxes",
-        //             "labelPosition": "top",
-        //             "optionsLabelPosition": "right",
-        //             "description": "",
-        //             "tooltip": "",
-        //             "customClass": "",
-        //             "tabindex": "",
-        //             "inline": false,
-        //             "hidden": false,
-        //             "hideLabel": false,
-        //             "autofocus": false,
-        //             "disabled": false,
-        //             "tableView": false,
-        //             "modalEdit": false,
-        //             "dataSrc": "values",
-        //             "values": [
-        //                 {
-        //                     "label": "test",
-        //                     "value": "test",
-        //                     "shortcut": ""
-        //                 }
-        //             ],
-        //             "valueProperty": "",
-        //             "persistent": true,
-        //             "protected": false,
-        //             "dbIndex": false,
-        //             "encrypted": false,
-        //             "clearOnHide": true,
-        //             "customDefaultValue": "",
-        //             "calculateValue": "",
-        //             "calculateServer": false,
-        //             "allowCalculateOverride": false,
-        //             "validate": {
-        //                 "required": false,
-        //                 "onlyAvailableItems": false,
-        //                 "customMessage": "",
-        //                 "custom": "",
-        //                 "customPrivate": false,
-        //                 "json": "",
-        //                 "strictDateValidation": false,
-        //                 "multiple": false,
-        //                 "unique": false
-        //             },
-        //             "errorLabel": "",
-        //             "minSelectedCountMessage": "",
-        //             "maxSelectedCountMessage": "",
-        //             "errors": "",
-        //             "key": "selectBoxes",
-        //             "tags": [],
-        //             "properties": {},
-        //             "conditional": {
-        //                 "show": null,
-        //                 "when": null,
-        //                 "eq": "",
-        //                 "json": ""
-        //             },
-        //             "customConditional": "",
-        //             "logic": [],
-        //             "attributes": {},
-        //             "overlay": {
-        //                 "style": "",
-        //                 "page": "",
-        //                 "left": "",
-        //                 "top": "",
-        //                 "width": "",
-        //                 "height": ""
-        //             },
-        //             "type": "selectboxes",
-        //             "data": {
-        //                 "url": ""
-        //             },
-        //             "template": "<span>{{ item.label }}</span>",
-        //             "redrawOn": "",
-        //             "input": true,
-        //             "placeholder": "",
-        //             "prefix": "",
-        //             "suffix": "",
-        //             "multiple": false,
-        //             "unique": false,
-        //             "refreshOn": "",
-        //             "dataGridLabel": false,
-        //             "widget": null,
-        //             "validateOn": "change",
-        //             "showCharCount": false,
-        //             "showWordCount": false,
-        //             "allowMultipleMasks": false,
-        //             "addons": [],
-        //             "authenticate": false,
-        //             "ignoreCache": false,
-        //             "inputType": "checkbox",
-        //             "fieldSet": false,
-        //             "id": "exwiq7",
-        //             "defaultValue": {}
-        //         },
-        //         {
-        //             "type": "button",
-        //             "label": "Submit",
-        //             "key": "submit",
-        //             "size": "md",
-        //             "block": false,
-        //             "action": "submit",
-        //             "disableOnInvalid": true,
-        //             "theme": "primary",
-        //             "id": "e0817m",
-        //             "input": true,
-        //             "placeholder": "",
-        //             "prefix": "",
-        //             "customClass": "",
-        //             "suffix": "",
-        //             "multiple": false,
-        //             "defaultValue": null,
-        //             "protected": false,
-        //             "unique": false,
-        //             "persistent": false,
-        //             "hidden": false,
-        //             "clearOnHide": true,
-        //             "refreshOn": "",
-        //             "redrawOn": "",
-        //             "tableView": false,
-        //             "modalEdit": false,
-        //             "dataGridLabel": true,
-        //             "labelPosition": "top",
-        //             "description": "",
-        //             "errorLabel": "",
-        //             "tooltip": "",
-        //             "hideLabel": false,
-        //             "tabindex": "",
-        //             "disabled": false,
-        //             "autofocus": false,
-        //             "dbIndex": false,
-        //             "customDefaultValue": "",
-        //             "calculateValue": "",
-        //             "calculateServer": false,
-        //             "widget": {
-        //                 "type": "input"
-        //             },
-        //             "attributes": {},
-        //             "validateOn": "change",
-        //             "validate": {
-        //                 "required": false,
-        //                 "custom": "",
-        //                 "customPrivate": false,
-        //                 "strictDateValidation": false,
-        //                 "multiple": false,
-        //                 "unique": false
-        //             },
-        //             "conditional": {
-        //                 "show": null,
-        //                 "when": null,
-        //                 "eq": ""
-        //             },
-        //             "overlay": {
-        //                 "style": "",
-        //                 "left": "",
-        //                 "top": "",
-        //                 "width": "",
-        //                 "height": ""
-        //             },
-        //             "allowCalculateOverride": false,
-        //             "encrypted": false,
-        //             "showCharCount": false,
-        //             "showWordCount": false,
-        //             "properties": {},
-        //             "allowMultipleMasks": false,
-        //             "addons": [],
-        //             "leftIcon": "",
-        //             "rightIcon": ""
-        //         }
-        //     ]
-        // }
         setEditBuilder(true)
       
     }, [editBuilder])
@@ -418,6 +62,46 @@ const EditBuilder = () => {
           })
     }
 
+
+    const customComponents = {
+      customTextField: {
+        title: 'First Name',
+        group: 'custom',
+        icon: 'fa-solid fa-user',
+        template: 'customTextField',
+        schema: {
+          type: 'textfield',
+          key: 'firstName',
+          label: 'First Name ',
+          input: true,
+        },
+      },
+      customMiddleNameTextField: {
+        title: 'Middle Name',
+        group: 'custom',
+        icon: 'fa-solid fa-user',
+        template: 'customTextField',
+        schema: {
+          type: 'textfield',
+          key: 'middleName',
+          label: 'Middle Name ',
+          input: true,
+        },
+      },
+      customLastNameTextField: {
+        title: 'Last Name',
+        group: 'custom',
+        icon: 'fa-solid fa-user',
+        template: 'customTextField',
+        schema: {
+          type: 'textfield',
+          key: 'laastName',
+          label: 'Last Name ',
+          input: true,
+        },
+      },
+    };
+
     return (
         <>
             <div style={{ display: 'flex' }}>
@@ -434,12 +118,12 @@ const EditBuilder = () => {
         options={{
           builder: {
             custom: {
-              basic: {
-                default: true,
-                components: require('./formbuilder.json')
-              }
-            }
-          }
+              title: 'Custom',
+              default: true,
+              weight: 0,
+              components: customComponents,
+            },
+          },
         }}
       />
             {/* <Card title="Form JSON Schema" className="my-4">
